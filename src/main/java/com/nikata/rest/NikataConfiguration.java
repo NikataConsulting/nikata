254f3213cf4cc3d4263eafa7b744eb26964324c4
legend <gauravoli85@gmail.com>
2017-05-14 13:24:19 -0400

    Adding Bean Configuration

--- src/main/java/com/nikata/rest/NikataConfiguration.java
+++ src/main/java/com/nikata/rest/NikataConfiguration.java
@@ -3,10 +3,37 @@
  */
 package com.nikata.rest;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.client.RestTemplate;
+
+import com.nikata.rest.constant.Property;
+
 /**
  * @author Gaurav Oli
  * @date May 13, 2017 10:19:27 PM
  */
+@Configuration
 public class NikataConfiguration {
+	@Autowired
+	private Property property;
 
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(property.getDriver());
+		dataSource.setUrl(property.getUrl());
+		dataSource.setUsername(property.getUsername());
+		dataSource.setPassword(property.getPassword());
+		return dataSource;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
