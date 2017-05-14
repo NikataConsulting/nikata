1ca66ae7c51089c7dc20c298614a397eb8c96d27
legend <gauravoli85@gmail.com>
2017-05-13 22:27:26 -0400

    EhCache Added

---
+++ src/main/java/com/nikata/rest/NikataConfiguration.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.nikata.rest;
+
+/**
+ * @author Gaurav Oli
+ * @date May 13, 2017 10:19:27 PM
+ */
+public class NikataConfiguration {
+
+}
---
+++ src/main/java/com/nikata/rest/controller/OTPController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.nikata.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Gaurav Oli
+ * @date May 13, 2017 10:16:13 PM
+ */
+@RestController
+public class OTPController {
+
+}
